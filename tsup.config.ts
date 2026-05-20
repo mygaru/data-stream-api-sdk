7928088d990665841d213a2d9aa65e1eecf84562
Vasylyshyn Vitaliia <vi.vasylyshyn@apiko.com>
2026-05-20 09:17:20 +0300

    feat(MYG-2338): Add iife support

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  tsconfig: "tsconfig.build.json",
-  sourcemap: true,
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  dts: true,
-  clean: true,
-  target: "es2020",
-  outDir: "dist",
-});
+export default defineConfig([
+  {
+    tsconfig: 'tsconfig.build.json',
+    sourcemap: true,
+    entry: { index: 'src/index.ts' },
+    format: ['esm'],
+    dts: true,
+    clean: true,
+    target: 'es2020',
+    outDir: 'dist',
+  },
+  {
+    tsconfig: 'tsconfig.build.json',
+    sourcemap: true,
+    entry: { browser: 'src/browser.ts' },
+    format: ['iife'],
+    platform: 'browser',
+    clean: false,
+    target: 'es2020',
+    outDir: 'dist',
+  },
+]);
